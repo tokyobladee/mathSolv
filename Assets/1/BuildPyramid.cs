fd2908e66ffbddd74c9d46fb4cb15900804e529d
trixie-dev <mckinley55x@gmail.com>
2024-07-05 18:15:43 +0300

    pyramide

--- Assets/1/BuildPyramid.cs
+++ Assets/1/BuildPyramid.cs
@@ -6,38 +6,27 @@ public class BuildPyramid : MonoBehaviour
 {
     public GameObject cube;
     public int amountLevel = 6;
-    private int side = 3;
-    private int prevSide = 1;
-    private Vector3 startPosition;
-    private Vector3 foo;
-
 
     public void Pyramide()
     {
         for (int i = 1; i < amountLevel; i++)
         {
-            for (int j = 0; j < side; j++)
+            int y = amountLevel - i;
+            int len = 2 * (i + 1) - 3;
+
+            for (int x = i - len; x < i; x++)
             {
-                for (int k = 0; k < side; k++)
+                for (int z = i - len; z < i; z++)
                 {
-                    Instantiate(cube, foo, Quaternion.identity);
-                    foo.z -= 1;
+                    Vector3 pos = new Vector3(x, y, z);
+                    Instantiate(cube, pos, Quaternion.identity);
                 }
-
-                foo.x -= 1;
             }
-
-            side += 2;
-            startPosition.Set(startPosition.x + 1, startPosition.y - 1, startPosition.z + 1);
-            foo = startPosition;
         }
     }
 
     public void Start()
     {
-        foo.Set(1, amountLevel - 1, 1);
-        startPosition.Set(cube.transform.position.x + 1, amountLevel - 1, cube.transform.position.z + 1);
-        Instantiate(cube, foo, Quaternion.identity);
         Pyramide();
     }
 }
